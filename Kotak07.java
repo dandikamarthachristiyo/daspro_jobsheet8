e66a9aa34e944e5473b3410c2bdbd89c73ee4eb1
dandikamarthachristiyo <dandika.martha@gmail.com>
2024-10-22 11:41:32 +0700

    jobsheet 8

---
+++ Kotak07.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Kotak07 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int n = sc.nextInt();
+
+        for(int k = 1; k <= n; k++){
+            System.out.println();
+            
+            for (int i = 1; i <= n; i++) {
+                if (k > 1 && k < n && i > 1 && i < n) {
+                    System.out.print(" ");
+                }else{
+                    System.out.print(n);
+                }
+                System.out.print(" ");
+            }
+        }
+    }
+}
